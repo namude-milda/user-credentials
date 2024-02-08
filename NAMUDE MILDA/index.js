f47bed7471e1860e1da57cb419f0551ca252c9b2
namude-milda <159454351+namude-milda@users.noreply.github.com>
2024-02-08 17:24:38 +0300

    Add files via upload

---
+++ NAMUDE MILDA/index.js
@@ -0,0 +1,6 @@
+function userCredentials(email,password){
+    if(email=="namudemilda@gmail.com" && password=="2200702345"){
+        console.log("login successfully")
+    } else{ console.log("incorrect user credentials")}
+}
+userCredentials("namudemilda@gmail.com","2200702345");
